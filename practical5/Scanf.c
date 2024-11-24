64c8001b32e326f661044e67d274f14984ce95d6
Emma-Thomas-jj <64660346+Emma-Thomas-jj@users.noreply.github.com>
2024-11-24 21:22:49 +0530

    Delete practical5 directory

--- practical5/Scanf.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(void) {
-    int i;
-    double val; 
-    printf("Enter an int and double\n");
-    scanf("%d %lf",&i,&val);
-    printf("The entered values are %d and %lf\n ", i, val);
-    }
